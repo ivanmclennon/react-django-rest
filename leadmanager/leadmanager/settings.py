c72b900d7e2f1eef76ae101e936386800e7cf0a7
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-07-11 20:15:07 +0300

    basic react implementation

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'leads',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATIC_ROOT = BASE_DIR
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'frontend'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
